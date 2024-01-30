b8a6b8263997bd878f7c2db14faf585d2fe796a8
Alex C-G <alexcg@outlook.com>
2024-01-30 16:08:47 +0100

    chore: clean up dead code
    Signed-off-by: Alex C-G <alexcg@outlook.com>

--- app.py
+++ app.py
@@ -2,18 +2,6 @@ import os
 
 from helper import GhostTagger
 
-# import yaml
-
-
-# if os.path.isfile(".env"):
-# from dotenv import load_dotenv
-
-# load_dotenv()
-
-# with open("config.yml") as file:
-# config = yaml.safe_load(file)
-# os.environ["GHOST_BLOG_URL"] = config["ghost_url"]
-
 GHOST_API_KEY = os.environ["GHOST_API_KEY"]
 GHOST_BLOG_URL = os.environ["GHOST_BLOG_URL"]
 SCENEX_API_KEY = os.environ["SCENEX_API_KEY"]
